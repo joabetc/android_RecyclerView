02f3ac5ff43c1a0fd9c271ccc217943b6b57e863
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-10-03 16:55:33 -0300

    Adding behavior to the FAB

--- app/src/main/java/com/joabe/recyclerview/MainActivity.java
+++ app/src/main/java/com/joabe/recyclerview/MainActivity.java
@@ -32,8 +32,10 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                int wordListSize = mWordList.size();
+                mWordList.addLast("+ Word " + wordListSize);
+                mRecyclerView.getAdapter().notifyItemInserted(wordListSize);
+                mRecyclerView.smoothScrollToPosition(wordListSize);
             }
         });
 
