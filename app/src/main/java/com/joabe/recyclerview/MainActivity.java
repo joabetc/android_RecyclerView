bd3afb22d1f60149636eedaab720a5161854a592
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-10-03 15:30:12 -0300

    Adding code to create data

--- app/src/main/java/com/joabe/recyclerview/MainActivity.java
+++ app/src/main/java/com/joabe/recyclerview/MainActivity.java
@@ -12,7 +12,10 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.LinkedList;
+
 public class MainActivity extends AppCompatActivity {
+    private final LinkedList<String> mWordList = new LinkedList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        for (int i = 0; i < 20; i++) {
+            mWordList.addLast("Word " + i);
+        }
     }
 
     @Override
